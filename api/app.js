660ae8a2cd207669fdd3d830ea6df034fb5e4f1c
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-26 11:10:06 -0200

    excluindo lixo e alterando a porta

--- api/app.js
+++ api/app.js
@@ -1,8 +1,6 @@
-
 const express = require('express');
 const app = express();         
 const bodyParser = require('body-parser');
-const port = 3000; //porta padrão
 
 
 //configurando o body parser para pegar POSTS caso precise
@@ -10,17 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //inicia o servidor
-app.listen(port);
+app.listen(3001);
 console.log('API funcionando!');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 app.use('/', router);
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
-
+//chama o banco de dados
 const execQuery = require ('./db');
-
 
 
 router.get('/beacons', (req, res) =>{
