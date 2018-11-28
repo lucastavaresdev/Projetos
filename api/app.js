b42e0381f7a1c207dd0ae287153684daaadd6f26
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-28 10:05:34 -0200

    query temperatura atual

--- api/app.js
+++ api/app.js
@@ -20,16 +20,12 @@ router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 //chama o banco de dados
 const execQuery = require ('./db');
 
+// ROTAS
 
-router.get('/beacons', (req, res) =>{
-    execQuery('SELECT * FROM beacons', res);
-})
 
-router.get('/usuarios', (req, res) =>{
-    execQuery('SELECT * FROM usuarios', res);
-})
+router.get('/hcor/beacons_temperatura_atual', (req, res) =>{
+    const $Temperatura_atual = 'SELECT e.nome as nome_do_beacon , b.beacon as mac_beacon , b.temp as temperatura , e.setor, s.nome as nome_setor FROM equipamentos as e  inner join beacons as b  inner join setores s where e.serie = b.beacon and e.setor = s.id';
 
-router.get('/agendamento', (req, res) =>{
-    execQuery('SELECT * FROM agendamento;', res);
+    execQuery($Temperatura_atual, res);
 })
 
