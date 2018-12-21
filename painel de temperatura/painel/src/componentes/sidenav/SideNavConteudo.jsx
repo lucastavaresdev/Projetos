7c3bf274f1e42815f83bf28ce881abc1fda85607
Lucas Tavares <lucastavaresdev@gmail.com>
2018-12-21 14:55:18 -0200

    tamanho da sidebar

--- painel de temperatura/painel/src/componentes/sidenav/SideNavConteudo.jsx
+++ painel de temperatura/painel/src/componentes/sidenav/SideNavConteudo.jsx
@@ -21,8 +21,8 @@ class Logo extends Component {
     render() {
         return (
             <div>
-                <div className='row logo-bg '>
-                    <div className='col-12'>
+                <div className='logo-bg '>
+                    <div className='col-8'>
                         <img src={logo2} width='220px' alt='Logo' className='logo_imagem' />
                     </div>
                     <div className="container">
