29b6cc6c3bc6ac0230661a88a0cdc5ea3defeea4
Lucas Tavares <lucastavaresdev@gmail.com>
2018-12-21 11:52:48 -0200

    Titulo Consolidado

--- painel de temperatura/painel/src/componentes/consolidado.jsx
+++ painel de temperatura/painel/src/componentes/consolidado.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Card from './cards/card'
+import Titulo from './navbar/navbar'
 
 class Consolidado extends Component {
 
     render() {
         return (
             <div>
+            { <Titulo titulo={'Consolidado'} /> }
                 <div className='container-fluid'>
                     <div className='row mt-5'>
                         <div className='col-md-4'>
