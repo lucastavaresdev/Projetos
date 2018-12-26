1f8e5eab13eabc9591886774e15605c260b4b3c7
Lucas Tavares <lucastavaresdev@gmail.com>
2018-12-26 11:17:30 -0200

    estilo consolidado alterado para deixar responsavel

--- painel de temperatura/painel/src/componentes/cards/card.jsx
+++ painel de temperatura/painel/src/componentes/cards/card.jsx
@@ -8,10 +8,10 @@ class Card extends Component {
             <div className="container py-3">
               <div className="card background-color">
                 <div className="row ">
-                  <div className="col-lg-4 col-md-5">
+                  <div className="col-lg-5 col-md-5">
                         <h1 className='text-center pl-3 pt-4 pb-3 align_vertical'>{this.props.temperatura_atual}ºC</h1>
                     </div>
-                    <div className="col-lg-8 col-md-7 info-local-setor" >
+                    <div className="col-lg-7 col-md-7 info-local-setor" >
                         <h2 className='text-center  align_vertical'>{this.props.equipamento}</h2>
                         <p className='text-center  align_vertical'>{this.props.setor}</p>
                     </div>
