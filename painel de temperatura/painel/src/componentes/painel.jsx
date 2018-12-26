b768450e3af9b21b442cba84b5d4c63f389e4ba5
Lucas Tavares <lucastavaresdev@gmail.com>
2018-12-26 11:22:21 -0200

    corigindo falhas para começar a colocar dados, mudado o nome da class

--- painel de temperatura/painel/src/componentes/painel.jsx
+++ painel de temperatura/painel/src/componentes/painel.jsx
@@ -8,40 +8,36 @@ import './_estilos_paginas/_dashboard.scss'
 import jwt_decode from 'jwt-decode'
 
 
- class Dashboard extends Component{
+ class Painel extends Component{
     
     componentDidMount () {
         const token = localStorage.usertoken
         const decoded = jwt_decode(token)
     }
 
-
         render(){
               return (
                         <div >
-                                            { <Titulo titulo={this.props.TitulodaPagina} /> }
-
-                                                <div className='container-fluid'>
-                                                    <div className="row bg_grafico_media">
-                                                        <div className='col-md-7 col-xs-12 mt-3 tamanho' >
-                                                            <p className='texto-branco text-center'>Temperatura</p>
-                                                            <Grafico />
-                                                        </div>
-                                                        <Temperatura_media />
-                                                    </div>
-                                              <div className="row mt-3">
-                                                <div className='col-md-7 tamanho' >
-                                                    <Tabela />
-                                                </div>
-                                                <Temperatura_Atual tempertatura_atual={this.props.mac} temperatura={this.props.temperatura}/>
-                                            </div>
+                               <Titulo titulo={this.props.TitulodaPagina} /> 
+                               <div className='container-fluid'>
+                                    <div className="row bg_grafico_media">
+                                        <div className='col-md-7 col-xs-12 mt-3 tamanho' >
+                                            <p className='texto-branco text-center'>Temperatura</p>
+                                            <Grafico />
                                         </div>
-                                    </div >
-
+                                        <Temperatura_media />
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className='col-md-7 tamanho' >
+                                            <Tabela />
+                                        </div>
+                                        <Temperatura_Atual tempertatura_atual={this.props.mac} temperatura={this.props.temperatura}/>
+                                    </div>
+                               </div>
+                        </div >
         )
     }
 }
 
 
-
-export default Dashboard
+export default Painel
--- painel de temperatura/painel/src/componentes/temperaturas/Temperatura_atual.jsx
+++ painel de temperatura/painel/src/componentes/temperaturas/Temperatura_atual.jsx
@@ -5,31 +5,13 @@ import axios from 'axios'
 
 class Temperatura_Atual extends Component {
     
-    constructor(props){
-        super(props)
-           this.state = {
-            temp: ''
-            }
-        }
-         
-        componentDidMount(){
-            setInterval( () => {
-                    axios.get(`http://localhost:3001/hcor/componente_temperatura_atual/${this.props.tempertatura_atual}`)
-                                .then((res)=>{
-                                    this.setState({
-                                    temp: res.data[0].temperatura
-                                })
-                            })
-            },5000)
-        }
-    
+
 
     render(){
-        const {temp} = this.state
         return(
             <div className='col-md-5 tamanho text-center' >
                     <h1 className='mt-5 '>Temperatura Atual</h1>
-                    <h3 className='titulo_temperatura_atual p-1'>{temp} °C</h3>
+                    <h3 className='titulo_temperatura_atual p-1'>21C</h3>
             </div>
         )
     }
