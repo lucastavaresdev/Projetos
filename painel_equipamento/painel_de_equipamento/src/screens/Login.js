c9b6a8cb28400f9fa4c7b26e72e1fc04d86cc6aa
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-10 17:01:32 -0200

    tela de login em construção

--- painel_equipamento/painel_de_equipamento/src/screens/Login.js
+++ painel_equipamento/painel_de_equipamento/src/screens/Login.js
@@ -53,11 +53,29 @@ onSubmit(e){
                   <div className="card-header card-topo text-center"> <img src={Logo} /> </div>
 
                   <div className='row justify-content-center'>
-                      <div className='col-9'>
+                      <div className='col-10'>
                             <form noValidate onSubmit={this.onSubmit}>
+                            
+                      <div className='row'>
+                            <div className='col-1'>
+                              <i class="fas fa-user"></i>
+                            </div>
+                            <div className='col-11'>
                                   <input type="text"  className="form-control mb-3" name="usuario" placeholder="Usuario" value={this.state.usuario} onChange={this.onChange} />
+                            </div>
+                      </div>
+                      <div className='row'>
+                            <div className='col-1'>
+                            <i class="fas fa-lock"></i>
+                            </div>
+                            <div className='col-11'>
                                   <input type="password" className="form-control mb-3" name="senha" placeholder="Senha" value={this.state.senha} onChange={this.onChange} />
-                                  <button type="submit" className="btn btn-lg btn-primary btn-block">Entrar</button>
+                            </div>
+                      </div>
+                      <div className='text-center mt-4'>
+                                  <button type="submit" className="btn cor-botao col-5">Entrar</button>
+                        </div>
+
                               </form>
                         </div>
                       </div>
