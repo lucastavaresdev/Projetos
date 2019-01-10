58fa3b2d21d0474a3906e73e87ad046f904afa96
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-10 15:20:39 -0200

    login funcionando

--- painel_equipamento/painel_de_equipamento/src/screens/Login.js
+++ painel_equipamento/painel_de_equipamento/src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Login.css';
+import './Login.scss';
 import {login} from '../Funcions'
 import jwt_decode from 'jwt-decode'
 
@@ -13,32 +13,35 @@ class Login extends Component {
         senha: ''
     }
 
-this.onChange = this.onChange.bind(this)
-this.onSubmit = this.onSubmit.bind(this)
-}
+  this.onChange = this.onChange.bind(this)
+  this.onSubmit = this.onSubmit.bind(this)
+  }
 
-onChange(e){
-this.setState({[e.target.name]: e.target.value})
-}
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
 
 onSubmit(e){
-e.preventDefault()
+    e.preventDefault()
 
-const user = {
-  usuario: this.state.usuario,
-  senha: this.state.senha
-}
-
-login(user).then(res => {
-    if(res){
-        const decoded = jwt_decode(res)
-        if(decoded.perfil === 0){
-            this.props.history.push('/admin')
-        }else if(decoded.perfil === 1){
-            this.props.history.push('/usuario')
-        }
+    const user = {
+      usuario: this.state.usuario,
+      senha: this.state.senha
     }
-})
+
+    login(user).then(res => {
+        if(res){
+            const decoded = jwt_decode(res)
+
+          var perfil = parseInt(decoded.perfil)
+
+            if(perfil === 0){
+                this.props.history.push('/admin')
+            }else if(perfil === 1){
+                this.props.history.push('/usuario')
+            }
+        }
+    })
 }
   
   render() {
@@ -46,7 +49,8 @@ login(user).then(res => {
       <div className="App">
         <header className="App-header">
          
-        <form noValidate onSubmit={this.onSubmit}>
+
+         <form noValidate onSubmit={this.onSubmit}>
                             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                             <div className="form-group">
                             <input type="text"     
