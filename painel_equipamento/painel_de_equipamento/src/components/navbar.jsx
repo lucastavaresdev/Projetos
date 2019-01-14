04a3bb0035048570ce617e11940a800956601b60
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-14 10:51:26 -0200

    navar incluindo as alterações

--- painel_equipamento/painel_de_equipamento/src/components/navbar.jsx
+++ painel_equipamento/painel_de_equipamento/src/components/navbar.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import './_navbar.scss'
 import Logo from '../images/logo.png'
-
+import Sidebar from "react-sidebar";
 
 class Navbar extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          sidebarOpen: false
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+      }
+
+
+      onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+      }
+
 
     sair(e) {
         e.preventDefault()
@@ -13,10 +27,24 @@ class Navbar extends Component {
 
     render() {
         return (
+            <div>
             <nav class="navbar">
             <img src={Logo} alt="Logo Itechmed soluções medicas" srcset=""/>
                 <a href="" onClick={this.sair.bind(this)}><i class="fas fa-times-circle"></i>Sair </a>
-      </nav>
+            </nav>
+       
+        <Sidebar sidebar={<b>Sidebar content dsadsadsdsa</b>} 
+            open={this.state.sidebarOpen} 
+            onSetOpen={this.onSetSidebarOpen}  
+            styles={{ sidebar: { background: "white" } }}
+            rootClassName = 'sidebar'
+            >
+
+
+                        <button onClick={() => this.onSetSidebarOpen(true)}> Open sidebar </button>
+         </Sidebar>
+
+      </div>
         );
     }
 }
