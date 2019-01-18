ec2f678d7d537ef05757f0a7b28a884fc26a59aa
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-18 14:56:19 -0200

    modal na estrutura

--- painel_equipamento/painel_de_equipamento/src/components/modal.jsx
+++ painel_equipamento/painel_de_equipamento/src/components/modal.jsx
@@ -1,57 +1,64 @@
-import React, {Component} from 'react';
-import Modal from 'react-modal';
- 
+import React, { Component } from 'react';
+import Modal from 'react-modal'
+
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
- 
-Modal.setAppElement(document.getElementById('Inserir'))
- 
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
 class Modal2 extends Component {
-  constructor() {
-    super();
-      this.state = {
-        modalIsOpen: false
-      };
- 
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
- 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
- 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
- 
-  render() {
-    return (
-      <div>
-        <div onClick={this.openModal}><i className="fas fa-plus-circle fa-5x"></i></div>
-        <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} style={customStyles} contentLabel="Inserir Equipamento">
-          <div className="container">
-            <div className="row">
-                <div className="col-11">
-                    <h2 className='text-center' >Inserir Equipamentos</h2>
+   
+    constructor() {
+        super();
+          this.state = {
+            modalIsOpen: false
+          };
+     
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+      }
+     
+      openModal() {
+        this.setState({modalIsOpen: true});
+      }
+     
+      closeModal() {
+        this.setState({modalIsOpen: false});
+      }
+
+    render() {
+        return (
+            <div>
+                 <div onClick={this.openModal}><i class="fas fa-plus-circle fa-3x"></i></div> 
+                    <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} style={customStyles} contentLabel="Inserir Equipamento">
+                    <div className="container-fluid">
+                                <div className="col-11">
+                                    <h2 className='text-center' >Inserir Equipamentos</h2>
+                                </div>
+                                <div className="col-1">
+                                    <div  onClick={this.closeModal}><i className="fas fa-times"></i></div>
+
+                            <div className="row">
+                                
+                                <form>
+                                    First name:
+                                    <input type="text" name="firstname" />
+                                </form>
+                                
+                           </div>
+                           </div>
+
+                        </div>
+                    </Modal>
                 </div>
-                <div className="col-1">
-                    <div  onClick={this.closeModal}><i className="fas fa-times"></i></div>
-                </div>
-            </div>
-          </div>
-        </Modal>
-      </div>
-    );
-  }
+        );
+    }
 }
- 
-export default Modal2
+
+export default Modal2;
--- painel_equipamento/painel_de_equipamento/src/screens/Admin.jsx
+++ painel_equipamento/painel_de_equipamento/src/screens/Admin.jsx
@@ -1,6 +1,4 @@
 import React , {Component}from 'react';
-import jwt_decode from 'jwt-decode'
-import Navbar from '../components/navbar';
 import './scss/_Admin.scss'
 import Modal2 from '../components/Modal'
 
@@ -12,22 +10,9 @@ class Admin extends Component {
 
     render () {
         return (
-            <div className='Container'>
-                <div className="row">
-                    <div className="col-md-4">
-                        <Modal2 />
-                    </div>
-                    <div className="col-md-4">
-                        <p>2</p>
-                    </div>
-                    <div className="col-md-4">
-                        <p>3</p>
-                    </div>
-              
-
-
+                <div>   
+                        <Modal2/>
                 </div>
-            </div>
         )
     }
 }
