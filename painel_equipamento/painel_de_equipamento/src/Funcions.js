84abd6a53c1dd5e872d269313f29aa75534256a7
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-21 15:51:50 -0200

    modal realizando o insert

--- painel_equipamento/painel_de_equipamento/src/Funcions.js
+++ painel_equipamento/painel_de_equipamento/src/Funcions.js
@@ -1,17 +1,5 @@
 import axios from 'axios'
 
-// export const register = newUser => {
-//     return axios.post('users/register', {
-//         first_name: newUser.first_name,
-//         last_name: newUser.last_name,
-//         email: newUser.email,
-//         password: newUser.password,
-//     })
-//     .then(res => {
-//         console.log("Registered")
-//     })
-// }
-
 export const login = user => {
     return axios
         .post('http://localhost:3003/equipamento/login', {
@@ -26,3 +14,23 @@ export const login = user => {
             console.log(err)
         })
 }
+
+export const Inserir_Equipamento = equipamento => {
+    return axios.post('http://localhost:3003/equipamento/cadastro_de_equipamento', {
+        nome: equipamento.nome,
+        marca: equipamento.marca,
+        modelo: equipamento.modelo,
+        serie: equipamento.serie,
+        patrimonio: equipamento.patrimonio,
+        ronda: equipamento.ronda,
+        calibracao: equipamento.calibracao,
+        situacao: equipamento.situacao,
+        ativo: equipamento.ativo,
+        setor: equipamento.setor,
+    })
+    .then(res => {
+         console.log("Registrado")
+    }).catch(function (error) {
+        console.log(error);
+    });
+  }
--- painel_equipamento/painel_de_equipamento/src/components/modal.jsx
+++ painel_equipamento/painel_de_equipamento/src/components/modal.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal'
+import {Inserir_Equipamento} from '../Funcions'
 import './_modal.scss'
 
 
@@ -19,20 +20,60 @@ class Modal2 extends Component {
     constructor() {
         super();
           this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            nome: ' ',
+            marca: ' ',
+            modelo: ' ',
+            serie: 0,
+            patrimonio: '',
+            ronda: '',
+            calibracao: 0,
+            situacao:  ' ',
+            ativo: ' ',
+            setor: 0,
           };
      
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
       }
+
      
-      openModal() {
-        this.setState({modalIsOpen: true});
-      }
+    openModal() {
+      this.setState({modalIsOpen: true});
+    }
      
-      closeModal() {
+    closeModal() {
         this.setState({modalIsOpen: false});
-      }
+    }
+      
+    onChange (e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit (e) {
+        e.preventDefault()
+
+        const equipamento = {
+            nome: this.state.nome,
+            marca: this.state.marca,
+            modelo:this.state.modelo,
+            serie: this.state.serie,
+            patrimonio: this.state.patrimonio,
+            ronda: this.state.ronda,
+            calibracao: this.state.calibracao,
+            situacao:  this.state.situacao,
+            ativo: this.state.ativo,
+            setor: this.state.setor,
+        }
+
+         
+        Inserir_Equipamento(equipamento).then(res => {
+                this.closeModal();
+        })
+    }
+
 
     render() {
         return (
@@ -50,24 +91,27 @@ class Modal2 extends Component {
                                                     </div>
                                                 </div>
                                             </div>
-                                         <form className='formulario_cadastro_equipamento'>
+                                         <form className='formulario_cadastro_equipamento' onSubmit={this.onSubmit}>
                                             <p className='col-12' >Dados do Equipamento</p>
 
                                                 <label className="col-md-9 col-sm-12">Nome do Equipamento:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="nome" value={this.state.nome}  onChange={this.onChange} required/>
                                                 </label>
+
                                                 <label className="col-md-3 col-sm-12">Marca:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="marca"  value={this.state.marca}  onChange={this.onChange} required/>
                                                 </label>
 
                                                 <label className="col-md-4 col-sm-12"> Modelo:
-                                                    <input className="col-12" type="text" name="nome" />
+                                                    <input className="col-12" type="text" name="modelo" value={this.state.modelo}  onChange={this.onChange} required/>
                                                 </label>
+
                                                 <label className="col-md-4 col-sm-12"> serie:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="serie" value={this.state.serie}  onChange={this.onChange} required/>
                                                 </label>
+
                                                 <label className="col-md-4 col-sm-12">   Patrimonio:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="patrimonio"  value={this.state.patrimonio}  onChange={this.onChange} required/>
                                                 </label>
 
                                                 <hr/>
@@ -75,17 +119,14 @@ class Modal2 extends Component {
                                                 <p className='col-12' >Dados para o beacon</p>
 
                                                 <label className="col-md-4 col-sm-12">   Ronda:
-                                                        <input  className="col-12" type="number" name="ronda" placeholder='Qtd. de Dias'/>
+                                                        <input  className="col-12" type="number" name="ronda" placeholder='Qtd. de Dias' value={this.state.ronda}  onChange={this.onChange} required/>
                                                 </label>
                                                 <label className="col-md-4 col-sm-12">  Calibração:
-                                                        <input  className="col-12" type="number" name="calibracao" placeholder='Qtd. de Dias'/>
+                                                        <input  className="col-12" type="number" name="calibracao" placeholder='Qtd. de Dias' value={this.state.calibracao}  onChange={this.onChange} required/>
                                                 </label>
-
-                                            
-                                                
-                                              
+                                                                                         
                                                 <label className="col-md-4 col-sm-12">   Situação:
-                                                        <select className='col-12'>
+                                                        <select className='col-12' name="situacao"  value={this.state.situacao}  onChange={this.onChange}>
                                                             <option value="0">Disponivel</option>
                                                             <option value="1">Utilizado</option>
                                                             <option value="2">Necessita Manutenção</option>
@@ -97,24 +138,20 @@ class Modal2 extends Component {
                                                         </select>
                                                 </label>
 
-
                                                 <label className="col-md-4 col-sm-12">  Ativo:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="ativo" value={this.state.ativo}  onChange={this.onChange} required/>
                                                 </label>
                                                 <label className="col-md-4 col-sm-12"> Setor:
-                                                    <input  className="col-12" type="text" name="nome" />
+                                                    <input  className="col-12" type="text" name="setor" value={this.state.setor}  onChange={this.onChange} required/>
                                                 </label>
 
                                                 <div className="col-12 mt-2">
-                                                    <input  className="offset-md-10 col-md-2 col-sm-12" type="submit" value="Cadastrar" />
+                                                    <input   type="submit" className="offset-md-10 col-md-2 col-sm-12" value="Cadastrar" />
                                                 </div>
 
                                             </form>
                                          </div>
-
-                                            <div className="col-md-2 modal_imagem">
-                                            
-                                            </div>
+                                         <div className="col-md-2 modal_imagem"></div>
                                          </div>
                                 </Modal>
                 </section>
