950ceca81981cc3b279bb0d35d4895948829d256
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-22 09:44:37 -0200

    .

--- painel_equipamento/api/database/db.js
+++ painel_equipamento/api/database/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize("hmcp", "itechflow@itechbd", "Itechm@ster_2018", {
-    host: 'itechbd.mysql.database.azure.com',
+
     dialect: 'mysql',
     operatorsAliases: false,
 
