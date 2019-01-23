440b5ecb0f4a80c0a42ff83410e97f251149467e
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-23 17:37:56 -0200

    tabela funcionando

--- painel_equipamento/painel_de_equipamento/src/screens/equipamentos.jsx
+++ painel_equipamento/painel_de_equipamento/src/screens/equipamentos.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {tracking_equipamentos} from '../Funcions'
  import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import '../../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
 
 
 class Equipamento extends Component {
@@ -16,18 +17,67 @@ class Equipamento extends Component {
     })
   }
 
+  componentDidMount(){
+    this.listar()
+  }
 
   render() {
     
-    this.listar()
+   
 
     const { SearchBar } = Search;
     const columns = [{
-      dataField: 'marca',
-      text: 'marca'
-    },];
+                                      dataField: 'gateway',
+                                      text: 'Gateway'
+                                    },
+                                    {
+                                      dataField: 'setor',
+                                      text: 'setor'
+                                    },
+                                    {
+                                      dataField: 'rssi',
+                                      text: 'rssi'
+                                    },
+                                    {
+                                      dataField: 'nome',
+                                      text: 'nome'
+                                    },
+                                    {
+                                      dataField: 'marca',
+                                      text: 'marca'
+                                    },
+                                    {
+                                      dataField: 'serie',
+                                      text: 'serie'
+                                    },
+                                    {
+                                      dataField: 'patrimonio',
+                                      text: 'patrimonio'
+                                    },
+                                    {
+                                      dataField: 'minor',
+                                      text: 'minor'
+                                    },
+                                    {
+                                      dataField: 'minor',
+                                      text: 'minor'
+                                    },
+                                    {
+                                      dataField: 'checkin',
+                                      text: 'checkin'
+                                    },
+                                    {
+                                      dataField: 'checkout',
+                                      text: 'checkout'
+                                    },
+                                    {
+                                      dataField: 'tempo',
+                                      text: 'tempo'
+                                    },
+  ];
     
     
+  
   return (
     <div className='container-fluid p-5'>
           <ToolkitProvider keyField="id" data={  this.state.data } columns={ columns } search >
@@ -38,9 +88,7 @@ class Equipamento extends Component {
                   <h3>Tracking_equipamentos</h3>
                   <SearchBar { ...props.searchProps } />
                   <hr />
-                  <BootstrapTable
-                    { ...props.baseProps }
-                  />
+                  <BootstrapTable rowStyle={ { backgroundColor: 'red' } }  { ...props.baseProps }  />
                 </div>
               )
             }
