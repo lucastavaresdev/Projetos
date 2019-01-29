95684649b58f647e8f2738a2d3ccfadb73cc372e
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-29 11:27:19 -0200

    Click funcionando e trazendo os componentes

--- painel_equipamento/painel_de_equipamento/src/screens/Admin.jsx
+++ painel_equipamento/painel_de_equipamento/src/screens/Admin.jsx
@@ -3,49 +3,73 @@ import './scss/_Admin.scss'
 import '../components/_card.scss'
 import Card from '../components/card'
 import { quantidade_de_equipamento } from '../Funcions'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 
-const data = [
-        {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-        {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-        {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-        {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-        {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-        {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-        {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-  ];
-
+      
 class Admin extends Component {
         constructor(){
                 super()
                 this.state = { 
                         quantidade_de_equipamento  : 0,
                         em_manutencao  : 0,
-                        varificados  : 0
+                        varificados  : 0,
+                        equipamento_visivel: false,
+                        ronda_visivel: false,
+                        calibracao_visivel: false
                 };
+        }
+
+        componentDidMount(){
+                document.querySelector('.screens').style.display = 'block';
+                this.exibir_quantidade_de_equipamento()
         }
 
         exibir_quantidade_de_equipamento() {
                 quantidade_de_equipamento().then(json => {
-                        console.log(json.data[0]);
                         this.setState({ quantidade_de_equipamento: json.data[0].quantidade_de_equipamentos})
                 })
         }
 
-        equipamentos = (param) => {
-                console.log(param);
+
+        ExibirEquipamento = () => {
+                (this.state.equipamento_visivel === false) ? 
+                        this.setState({equipamento_visivel: true,  ronda_visivel: false, calibracao_visivel:false}): this.setState({equipamento_visivel: false });
+        }
+     
+        ExibirRonda = () => {
+                (this.state.ronda_visivel === false) ? 
+                        this.setState({ronda_visivel: true, equipamento_visivel: false, calibracao_visivel:false}) : this.setState({ronda_visivel: false });
+        }
+
+        ExibirCalibracao = () => {
+                (this.state.calibracao_visivel === false) ? 
+                        this.setState({calibracao_visivel: true ,ronda_visivel: false, equipamento_visivel: false}) : this.setState({calibracao_visivel: false });
         }
 
         
+        ComponenteEquipamentos = () => {
+                if (!this.state.equipamento_visivel) return null;
+                return (
+                <h1>Equipamentos</h1>
+                );
+        }
 
-     componentDidMount(){
-        document.querySelector('.screens').style.display = 'block';
-        this.exibir_quantidade_de_equipamento()
-      }
+        ComponenteRonda = () => {
+                if (!this.state.ronda_visivel) return null;
+                return (
+                        <h1>Ronda</h1>
+                );
+        }
+                
+        ComponenteCalibracao = () => {
+                if (!this.state.calibracao_visivel) return null;
+                return (
+                        <h1>Calibração</h1>
+                );
+        }
+                
 
       render () {
-  
         return (
                 <div className="container-fluid">
                         <div className="row ">
@@ -53,22 +77,18 @@ class Admin extends Component {
                                 <h1 className='mt-70 titulo_das_paginas titulo_left '>Dashboard</h1>
                                 </div>
                         </div>
-                        
-                        
+                                                        
                         <div className="row mt-2">
-                                <div className="col-md-4 col-sm-12"  onClick={this.equipamentos('Equipamento')}>
+                                <div className="col-md-4 col-sm-12"  onClick={this.ExibirEquipamento}>
                                         <Card  nome='Equipamentos' qtd={this.state.quantidade_de_equipamento} icone='tools' click='click'/>
                                 </div>
-                                <div className="col-md-4 col-sm-12">
-                                        <Card  nome='Rondas' qtd={this.state.em_manutencao} icone='retweet'/>
+                                <div className="col-md-4 col-sm-12" onClick={this.ExibirRonda}>
+                                        <Card  nome='Rondas' qtd={this.state.em_manutencao} icone='retweet' click='click'/>
                                 </div>
-                                <div className="col-md-4 col-sm-12">
-                                        <Card  nome='Calibração' qtd={this.state.varificados} icone='cog'/>
+                                <div className="col-md-4 col-sm-12" onClick={this.ExibirCalibracao}>
+                                        <Card  nome='Calibração' qtd={this.state.varificados} icone='cog' click='click'/>
                                 </div>
                         </div>
-
-
-
 
                         <div className="row mt-5">
                                 <div className="col-md-8 col-sm-12">
@@ -79,23 +99,9 @@ class Admin extends Component {
                                                         </div>
                                                         <div className="row pt-5">
                                                                 <div className="col-md-5">
-
-
-
-
-
-
-                                                                        {/* <ResponsiveContainer aspect={1.5}>
-                                                                                <LineChart data={data}>
-                                                                                <XAxis dataKey="name"/>
-                                                                                <YAxis/>
-                                                                                <CartesianGrid strokeDasharray="3 3"/>
-                                                                                <Tooltip/>
-                                                                                <Legend />
-                                                                                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-                                                                                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                                                                                </LineChart>
-                                                                        </ResponsiveContainer> */}
+                                                                {this.ComponenteCalibracao()}
+                                                                {this.ComponenteEquipamentos()}
+                                                                {this.ComponenteRonda()}
                                                                 </div>
                                                         </div>
                                                 </div>
