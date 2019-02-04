abbff26a746dfb27cbda759c9d50819cb85cb9c8
Lucas Tavares <lucastavaresdev@gmail.com>
2019-02-04 16:24:57 -0200

    query equipamentos

--- painel_equipamento/painel_de_equipamento/src/Funcions.js
+++ painel_equipamento/painel_de_equipamento/src/Funcions.js
@@ -52,3 +52,7 @@ export const quantidade_de_calibracoes = equipamento => {
 export const quantidade_de_rondas = equipamento => {
     return axios.get('http://localhost:3003/equipamento/rondas_status')
 }
+
+export const quantidade_de_equipamentos = equipamento => {
+    return axios.get('http://localhost:3003/equipamento/rondas_status')
+}
